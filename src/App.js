7975eba7aa53f4b9b16bd45e82bc43d9188bacd0
Zourunfa <473023989@qq.com>
2024-10-01 19:56:25 +0800

    chore: init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+import { observable } from 'mobx'
+
+// 初始化mobx容器仓库
+// 在组件种使用mbox容器状态
+// 在组件中发起action修改容器状态
+
+class Store {
+  @observable count = 0
+}
 
 function App() {
   return (
@@ -9,17 +19,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
